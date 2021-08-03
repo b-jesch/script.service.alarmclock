0206b8cfbc7879592a54b950b76aef6b36c3ddd6
b-jesch <birger.jesch@gmail.com>
2021-08-03 20:12:38 +0200

    Initial commit

---
+++ default.py
@@ -0,0 +1,126 @@
+"""
+Startup script for the Kodi alarm clock add-on.
+"""
+
+import os
+import xbmcvfs
+from resources.lib.cronjobs import *
+media = os.path.join(xbmcvfs.translatePath(addon.getAddonInfo('path')), 'resources', 'media', 'alert.mp3')
+
+
+def _stop_playing():
+    """
+    Stops whatever is playing at the moment
+    """
+    xbmc.Player().stop()
+
+
+def _start_playing(item, volume):
+    """
+    Starts playing the given item at the supplied volume.
+    """
+    try:
+        xbmc.executebuiltin('CECActivateSource')
+    except Exception:
+        log("CECActivateSource not supported", xbmc.LOGERROR)
+
+    xbmc.executebuiltin('SetVolume(%s)' % volume)
+    xbmc.Player().play(item)
+
+
+def get_jobs(number):
+    """
+    Initialize jobs(s) for alarm number.
+    If the alarm has a duration enabled, both the start and the stop job
+    are returned in the list.
+    """
+    days_of_week = int(addon.getSetting("day%d" % number))
+    if days_of_week == 7:
+        days_of_week = range(5)
+    if days_of_week == 8:
+        days_of_week = range(7)
+
+    action = addon.getSetting("action%d" % number)
+    if action == "0":
+        file_name = addon.getSetting("file%d" % number) if addon.getSetting("file%d" % number) != 'alert.mp3' else media
+    else:
+        file_name = addon.getSetting("text%d" % number)
+
+    jobs = [Job(number,
+                _start_playing,
+                int(addon.getSetting("minute%d" % number)),
+                int(addon.getSetting("hour%d" % number)),
+                dow=days_of_week,
+                args=[file_name, addon.getSetting("volume%d" % number)])]
+
+    if addon.getSetting("turnOff%d" % number) == "true":
+        jobs.append(Job(number,
+                        _stop_playing,
+                        int(addon.getSetting("minute%d" % number)) +
+                        (int(addon.getSetting("duration%d" % number)) % 60),
+                        int(addon.getSetting("hour%d" % number)) +
+                        (int(addon.getSetting("duration%d" % number)) / 60),
+                        dow=days_of_week))
+    return jobs
+
+
+def get_alarms():
+    """
+    Get a list of the cron jobs for the enabled alarms.
+    """
+    jobs = []
+    j = 0
+    for i in range(1, 6):
+        if addon.getSetting("alarm%d" % i) == "true":
+            jobs.extend(get_jobs(i))
+            j += 1
+    log("%s active event(s) fetched" % j)
+    return jobs
+
+
+class AlarmClock(object):
+    """
+    Main alarm clock application class.
+    """
+    def __init__(self):
+        self.crontab = CronTab()
+        self.monitor = self.AddonMonitor(self.crontab)
+
+    def apply_settings(self):
+        """
+        Gets the current configuration and updates the scheduler.
+        """
+        self.crontab.jobs = get_alarms()
+        log('Settings loaded')
+
+    def start(self):
+        """
+        Starts the alarm clock, ie. activates the defined alarms.
+        """
+        self.apply_settings()
+        self.crontab.start()
+
+    def stop(self):
+        """
+        Stops the alarm clock.
+        """
+        self.crontab.stop()
+
+    class AddonMonitor(xbmc.Monitor):
+        """
+        Monitoring class
+        """
+
+        def __init__(self, crontab):
+            xbmc.Monitor.__init__(self)
+            self.crontab = crontab
+
+        def onSettingsChanged(self):
+            self.crontab.jobs = get_alarms()
+            log('Settings applied')
+
+
+if __name__ == '__main__':
+    alarm_clock = AlarmClock()
+    log("Starting alarm clock...")
+    alarm_clock.start()
---
+++ resources/lib/cronjobs.py
@@ -0,0 +1,74 @@
+"""
+Cronjob like scheduling abstraction in Python.
+"""
+
+from datetime import datetime, timedelta
+import xbmc
+import xbmcaddon
+
+addon = xbmcaddon.Addon()
+addon_id = '[%s-%s]' % (addon.getAddonInfo('id'), addon.getAddonInfo('version'))
+
+
+def log(message, level=xbmc.LOGDEBUG):
+    xbmc.log('%s %s' % (addon_id, message), level)
+
+
+class CronTab(object):
+
+    """
+    Simulates basic cron functionality by checking for firing jobs every minute.
+    """
+
+    def __init__(self):
+        self.jobs = []
+        self.__enabled = True
+
+    def stop(self):
+        """
+        Stops the crontab.
+        """
+        self.__enabled = False
+
+    def start(self):
+        """
+        Starts to check every minute, if the registered jobs should run.
+        """
+        cron_time_tuple = datetime(*datetime.now().timetuple()[:5])
+        while self.__enabled and not xbmc.Monitor().abortRequested():
+            for job in self.jobs:
+                log("checking job #%s: (%02d:%02d [%s])" % (job.job_num, job.hours, job.mins, job.dow))
+                job.check(cron_time_tuple)
+            cron_time_tuple += timedelta(minutes=1)
+            if datetime.now() < cron_time_tuple:
+                xbmc.sleep((cron_time_tuple - datetime.now()).seconds * 1000)
+
+
+class Job(object):
+    """
+    Cron job abstraction.
+    """
+    def __init__(self, number, action, minute=None, hour=None, dow=None, args=(), kwargs=None):
+        self.job_num = int(number)
+        self.mins = int(minute)
+        self.hours = int(hour)
+        self.dow = dow
+        self.action = action
+        self.args = args
+        if kwargs is None: kwargs = {}
+        self.kwargs = kwargs
+
+    def is_matchtime(self, cron_time_tuple):
+        """
+        Is it the job's scheduled time
+        """
+        return ((cron_time_tuple.minute == self.mins) and
+                (cron_time_tuple.hour == self.hours) and
+                (cron_time_tuple.weekday() in self.dow))
+
+    def check(self, cron_time_tuple):
+        """
+        Checks if it is the scheduled time and executes the job if so.
+        """
+        if self.is_matchtime(cron_time_tuple):
+            self.action(*self.args, **self.kwargs)
